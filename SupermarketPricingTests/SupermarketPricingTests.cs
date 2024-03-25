2272397ebaa2772112a81a458e3cd6bf0e1af79c
Israel Desta <gisrael2023@gmail.com>
2024-03-25 18:51:53 +0000

    created a failing test that requires SuperMarket pricing to be imported

--- SupermarketPricingTests/SupermarketPricingTests.cs
+++ SupermarketPricingTests/SupermarketPricingTests.cs
@@ -2,14 +2,18 @@ namespace SupermarketPricingTests
 {
     public class SupermarketPricingTests
     {
+        private SupermarketPricing pricing;
+
         [SetUp]
         public void Setup()
         {
+           pricing = new SupermarketPricing();
         }
 
         [Test]
-        public void Test1()
+        public void Scan_OneItem_NospeicalPrice_mustReturnUnitPrice()
         {
+           
             Assert.Pass();
         }
     }
