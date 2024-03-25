bcd057324a8f6c3ffeb67236a41536961319214c
Israel Desta <gisrael2023@gmail.com>
2024-03-25 18:35:36 +0000

    Added a Test project SuperMarketPricingTest

---
+++ SupermarketPricingTests/SupermarketPricingTests.cs
@@ -0,0 +1,16 @@
+namespace SupermarketPricingTests
+{
+    public class SupermarketPricingTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
